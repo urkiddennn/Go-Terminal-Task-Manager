5a044304bbc1e38bbb9206a9df295026faaea207
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-06-05 16:37:41 +0800

    UPDATE: Added new functions for lisitng list and laoding list

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/pterm/pterm"
 	"github.com/pterm/pterm/putils"
@@ -14,21 +15,31 @@ type Task struct {
 }
 
 type TaskManager struct {
-	task Task
+	Tasks []Task
 }
 
 func main() {
-	pterm.DefaultBigText.WithLetters(putils.LettersFromStringWithStyle("TERMINAL", pterm.FgCyan.ToStyle()),
-		putils.LettersFromStringWithStyle("Task Manager", pterm.FgMagenta.ToStyle())).Render()
+	pterm.DefaultBigText.WithLetters(putils.LettersFromStringWithStyle("TERMINAL", pterm.FgCyan.ToStyle()), putils.LettersFromStringWithStyle("Task Manager", pterm.FgMagenta.ToStyle())).Render()
+	tm := loadTask()
+	listTask(tm)
+	for {
+		choice, _ := pterm.DefaultInteractiveSelect.WithOptions([]string{"Add Task", "List Task", "Mark Done", "Delete Task", "Exit"}).Show()
 
-	viewTask := TaskManager{
-		task: addTask(),
+		switch choice {
+		case "Add Task":
+			addTask(&tm)
+		case "List Task":
+			listTask(tm)
+		case "Mark Done":
+			markDone(&tm)
+		case "Exit":
+			break
+		}
 	}
-
-	fmt.Println(viewTask)
 }
 
-func addTask() Task {
+// Add new Task
+func addTask(tm *TaskManager) {
 	Name, _ := pterm.DefaultInteractiveTextInput.WithDefaultText("Enter Name Task").Show()
 
 	taskDes, _ := pterm.DefaultInteractiveTextInput.WithDefaultText("Enter Task Description").Show()
@@ -39,5 +50,39 @@ func addTask() Task {
 		taskDaone:       false,
 	}
 
-	return taskAdd
+	tm.Tasks = append(tm.Tasks, taskAdd)
+	pterm.Success.Println("Added new Task!")
+}
+
+// View list task
+func listTask(tm TaskManager) {
+	table := pterm.TableData{{"Task Name", "Task Description", "Task Done"}}
+	for _, task := range tm.Tasks {
+		taskBool := strconv.FormatBool(task.taskDaone)
+		table = append(table, []string{task.taskName, task.taskDescription, taskBool})
+	}
+	pterm.DefaultTable.WithHasHeader().WithData(table).Render()
+}
+
+func markDone(tm *TaskManager) {
+	var options []string
+
+	for _, tasks := range tm.Tasks {
+		options = append(options, tasks.taskName)
+	}
+
+	fmt.Println("All options: ", options)
+
+	selectedOption, _ := pterm.DefaultInteractiveSelect.WithOptions(options).Show()
+
+	fmt.Println("The selectedOption is ", selectedOption)
+	pterm.Info.Printfln("Selected option: %s", pterm.Green(selectedOption))
+}
+
+// Load Task
+func loadTask() TaskManager {
+	var tm TaskManager
+	fmt.Println(tm)
+
+	return tm
 }
