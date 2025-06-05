b982f603aa35558b9790918b9a6ccabc465a1715
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-06-05 19:06:47 +0800

    UPDATE: Added save task locally through json, add delete task

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"strconv"
 
 	"github.com/pterm/pterm"
@@ -9,17 +11,21 @@ import (
 )
 
 type Task struct {
-	taskName        string
-	taskDescription string
-	taskDaone       bool
+	TaskName        string `json:"taskName"`
+	TaskDescription string `json:"taskDescription"`
+	TaskDone        bool   `json:"taskDone"` // Fixed field name to match JSON tag
 }
 
 type TaskManager struct {
-	Tasks []Task
+	Tasks []Task `json:"tasks"`
 }
 
 func main() {
-	pterm.DefaultBigText.WithLetters(putils.LettersFromStringWithStyle("TERMINAL", pterm.FgCyan.ToStyle()), putils.LettersFromStringWithStyle("Task Manager", pterm.FgMagenta.ToStyle())).Render()
+	pterm.DefaultBigText.WithLetters(
+		putils.LettersFromStringWithStyle("TERMINAL", pterm.FgCyan.ToStyle()),
+		putils.LettersFromStringWithStyle("Task Manager", pterm.FgMagenta.ToStyle()),
+	).Render()
+
 	tm := loadTask()
 	listTask(tm)
 	for {
@@ -32,67 +38,136 @@ func main() {
 			listTask(tm)
 		case "Mark Done":
 			markDone(&tm)
+		case "Delete Task":
+			deleteTask(&tm) // Added placeholder for delete functionality
 		case "Exit":
-			break
+			if err := saveTask(tm); err != nil {
+				pterm.Error.Printfln("Failed to save tasks: %v", err)
+			} else {
+				pterm.Success.Println("Tasks saved successfully!")
+			}
+			return
 		}
 	}
 }
 
 // Add new Task
 func addTask(tm *TaskManager) {
-	Name, _ := pterm.DefaultInteractiveTextInput.WithDefaultText("Enter Name Task").Show()
-
+	name, _ := pterm.DefaultInteractiveTextInput.WithDefaultText("Enter Task Name").Show()
 	taskDes, _ := pterm.DefaultInteractiveTextInput.WithDefaultText("Enter Task Description").Show()
 
 	taskAdd := Task{
-		taskName:        Name,
-		taskDescription: taskDes,
-		taskDaone:       false,
+		TaskName:        name,
+		TaskDescription: taskDes,
+		TaskDone:        false,
 	}
 
 	tm.Tasks = append(tm.Tasks, taskAdd)
-	pterm.Success.Println("Added new Task!")
+	if err := saveTask(*tm); err != nil {
+		pterm.Error.Printfln("Failed to save task: %v", err)
+		return
+	}
+	pterm.Success.Println("Added new task!")
 }
 
-// View list task
+// View list of tasks
 func listTask(tm TaskManager) {
+	if len(tm.Tasks) == 0 {
+		pterm.Info.Println("No tasks available.")
+		return
+	}
 	table := pterm.TableData{{"Task Name", "Task Description", "Task Done"}}
 	for _, task := range tm.Tasks {
-		taskBool := strconv.FormatBool(task.taskDaone)
-		table = append(table, []string{task.taskName, task.taskDescription, taskBool})
+		taskBool := strconv.FormatBool(task.TaskDone)
+		table = append(table, []string{task.TaskName, task.TaskDescription, taskBool})
 	}
 	pterm.DefaultTable.WithHasHeader().WithData(table).Render()
 }
 
+// Mark task as done
 func markDone(tm *TaskManager) {
 	if len(tm.Tasks) == 0 {
-		pterm.Warning.Println("No task Avaailable")
+		pterm.Warning.Println("No tasks available")
 		return
 	}
 
 	var options []string
-	for _, tasks := range tm.Tasks {
-		options = append(options, tasks.taskName)
+	for _, task := range tm.Tasks {
+		options = append(options, task.TaskName)
 	}
 
 	selectedOption, _ := pterm.DefaultInteractiveSelect.WithOptions(options).Show()
 
 	for i, task := range tm.Tasks {
-		if task.taskName == selectedOption {
-			tm.Tasks[i].taskDaone = true
-			pterm.Success.Printfln("Task '%s' markded as done!", selectedOption)
+		if task.TaskName == selectedOption {
+			tm.Tasks[i].TaskDone = true
+			if err := saveTask(*tm); err != nil {
+				pterm.Error.Printfln("Failed to save tasks: %v", err)
+				return
+			}
+			pterm.Success.Printfln("Task '%s' marked as done!", selectedOption)
 			return
 		}
 	}
 	pterm.Error.Printfln("Task '%s' not found!", selectedOption)
-
-	pterm.Info.Printfln("Selected option: %s", pterm.Green(selectedOption))
 }
 
-// Load Task
-func loadTask() TaskManager {
-	var tm TaskManager
-	fmt.Println(tm)
+// Delete task (placeholder implementation)
+func deleteTask(tm *TaskManager) {
+	if len(tm.Tasks) == 0 {
+		pterm.Warning.Println("No tasks available")
+		return
+	}
 
+	var options []string
+	for _, task := range tm.Tasks {
+		options = append(options, task.TaskName)
+	}
+
+	selectedOption, _ := pterm.DefaultInteractiveTextInput.WithDefaultText("Enter Task Name to Delete").Show()
+
+	for i, task := range tm.Tasks {
+		if task.TaskName == selectedOption {
+			tm.Tasks = append(tm.Tasks[:i], tm.Tasks[i+1:]...)
+			if err := saveTask(*tm); err != nil {
+				pterm.Error.Printfln("Failed to save tasks: %v", err)
+				return
+			}
+			pterm.Success.Printfln("Task '%s' deleted!", selectedOption)
+			return
+		}
+	}
+	pterm.Error.Printfln("Task '%s' not found!", selectedOption)
+}
+
+// Load tasks from file
+func loadTask() TaskManager {
+	data, err := os.ReadFile("tasks.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			pterm.Info.Println("No tasks file found, starting with empty task list.")
+			return TaskManager{}
+		}
+		pterm.Error.Printfln("Error reading tasks file: %v", err)
+		return TaskManager{}
+	}
+
+	var tm TaskManager
+	if err := json.Unmarshal(data, &tm); err != nil {
+		pterm.Error.Printfln("Error parsing tasks file: %v", err)
+		return TaskManager{}
+	}
 	return tm
+}
+
+// Save tasks to file
+func saveTask(tm TaskManager) error {
+	data, err := json.MarshalIndent(tm, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling tasks: %v", err)
+	}
+	if err := os.WriteFile("tasks.json", data, 0644); err != nil {
+		return fmt.Errorf("error writing tasks file: %v", err)
+	}
+	return nil
 }
